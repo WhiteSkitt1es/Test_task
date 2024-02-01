6a7e23ae151105265227cf70f2609921b0306918
Pavel Artem'yev <pasha.artemev48@gmail.com>
2024-02-01 15:45:43 +0300

    Add new Dto

---
+++ src/main/java/com/whiteskittles/minesweeper/dto/NewGameDto.java
@@ -0,0 +1,12 @@
+package com.whiteskittles.minesweeper.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Value;
+
+@Value
+public class NewGameDto {
+    Integer width;
+    Integer height;
+    @JsonProperty("mines_count")
+    Integer minesCount;
+}
