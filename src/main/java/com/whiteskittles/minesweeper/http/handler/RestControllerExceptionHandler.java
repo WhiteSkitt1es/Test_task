3c87349e2221e746de78ef4d520ca5b83f79ecad
Pavel Artem'yev <pasha.artemev48@gmail.com>
2024-02-01 17:17:07 +0300

    Add postman tests

--- src/main/java/com/whiteskittles/minesweeper/http/handler/RestControllerExceptionHandler.java
+++ src/main/java/com/whiteskittles/minesweeper/http/handler/RestControllerExceptionHandler.java
@@ -6,13 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//@Slf4j
-//@RestControllerAdvice()
-//public class RestControllerExceptionHandler {
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<?> handlerException(Exception exception) {
-//        log.info(exception.getMessage());
-//        return ResponseEntity.badRequest().body(ErrorDto.of("Произошла непредвиденная ошибка"));
-//    }
-//}
+@Slf4j
+@RestControllerAdvice()
+public class RestControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handlerException(Exception exception) {
+        log.info(exception.getMessage());
+        return ResponseEntity.badRequest().body(ErrorDto.of("Произошла непредвиденная ошибка"));
+    }
+}
