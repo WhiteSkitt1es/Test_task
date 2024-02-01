820d4c0005e992ead6838f03b3be29005417071f
Pavel Artem'yev <pasha.artemev48@gmail.com>
2024-02-01 17:42:14 +0300

    Change exception handler

--- src/main/java/com/whiteskittles/minesweeper/http/handler/RestControllerExceptionHandler.java
+++ src/main/java/com/whiteskittles/minesweeper/http/handler/RestControllerExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.whiteskittles.minesweeper.http.handler;
 
 import com.whiteskittles.minesweeper.dto.ErrorDto;
+import com.whiteskittles.minesweeper.exceptions.EndGameException;
+import com.whiteskittles.minesweeper.exceptions.SessionException;
+import com.whiteskittles.minesweeper.exceptions.TableSizeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,5 +17,23 @@ public class RestControllerExceptionHandler {
     public ResponseEntity<?> handlerException(Exception exception) {
         log.info(exception.getMessage());
         return ResponseEntity.badRequest().body(ErrorDto.of("Произошла непредвиденная ошибка"));
+    }
+
+    @ExceptionHandler(EndGameException.class)
+    public ResponseEntity<?> endGameException(EndGameException exception) {
+        log.info(exception.getMessage());
+        return ResponseEntity.badRequest().body(ErrorDto.of("Игра закончена"));
+    }
+
+    @ExceptionHandler(SessionException.class)
+    public ResponseEntity<?> sessionException(SessionException exception) {
+        log.info(exception.getMessage());
+        return ResponseEntity.badRequest().body(ErrorDto.of("Неизвестная сессия"));
+    }
+
+    @ExceptionHandler(TableSizeException.class)
+    public ResponseEntity<?> tableSizeException(TableSizeException exception) {
+        log.info(exception.getMessage());
+        return ResponseEntity.badRequest().body(ErrorDto.of("Количество колонок и столбцов не равны"));
     }
 }
